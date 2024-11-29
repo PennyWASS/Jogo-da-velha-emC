630c9d9b8a161046c26b3e7e4908232ba3245160
Wálisson Andrey <85573798+PennyWASS@users.noreply.github.com>
2024-11-28 21:28:06 -0300

    Adicionando funções ao código

--- jogo da velha.cpp
+++ jogo da velha.cpp
@@ -14,6 +14,154 @@
 //		printf("\n");
 //	}
 
+// Implementações das funções
+void inicializarMatriz(char matriz[3][3]) {
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            matriz[i][j] = ' ';
+    	}
+	}
+}
+
+void exibirMatriz(char matriz[3][3]) {
+    printf("---------------------------------\n");
+    for(int i = 0; i < 3; i++){
+			for(int j = 0; j < 3; j++){
+				if (matriz[i][j] == ' '){
+					printf("\t*");
+				}else{
+					printf("\t%c", matriz[i][j]);
+				}
+			}
+			printf("\n\n");
+		}
+    printf("---------------------------------\n");
+}
+
+void exibirMenu() {
+    printf("O que você deseja fazer?\n");
+    printf("1- Jogar;\n2- Ver Ranking;\n3- Créditos;\n4- Sair.\n");
+}
+
+void exibirRanking(char jogador1[], char jogador2[], int placarA, int placarB) {
+    printf("---------------------------------\n");
+    printf("\t   Ranking:\n");
+    printf("---------------------------------\n");
+    if (strcmp(jogador1, " ") == 0) {
+        printf("Você ainda não jogou nenhuma partida\n\n");
+    } else {
+        printf("%s: %d pontos\n", jogador1, placarA);
+        if (strcmp(jogador2, " ") != 0) {
+            printf("%s: %d pontos\n", jogador2, placarB);
+        }
+    }
+}
+
+int verificarVencedor(char matriz[3][3], int *placarA, int *placarB) {
+	// Verifica linhas e colunas
+	
+	for (int i = 0; i < 3; i++) {
+	    if (matriz[i][0] == matriz[i][1] && matriz[i][1] == matriz[i][2] && matriz[i][0] != ' ') {
+	         // Linha completa
+	        if(matriz[i][0] == 'X'){
+	        	(*placarA)++;
+	        	return 1;
+			}else if(matriz[i][0] == 'O'){
+				(*placarB)++;
+				return 2;
+			}else{
+				return 3;
+			}
+	        
+	    }
+	    if (matriz[0][i] == matriz[1][i] && matriz[1][i] == matriz[2][i] && matriz[0][i] != ' ') {
+	        // Coluna completa
+	        if(matriz[0][i] == 'X'){
+	        	(*placarA)++;
+	        	return 1;
+			}else if(matriz[0][i] == 'O'){
+				(*placarB)++;
+				return 2;
+			}else{
+				return 3;
+			}
+	    }
+	}
+
+	// Verifica diagonais
+	if (matriz[0][0] == matriz[1][1] && matriz[1][1] == matriz[2][2] && matriz[0][0] != ' ') {
+	    // Diagonal principal
+	    if(matriz[0][0] == 'X'){
+	    	(*placarA)++;
+	    	return 1;
+		}else if(matriz[0][0] == 'O'){
+			(*placarB)++;
+			return 2;
+		}else{
+			return 3;
+		}
+	}
+	if (matriz[0][2] == matriz[1][1] && matriz[1][1] == matriz[2][0] && matriz[0][2] != ' ') {
+	    // Diagonal secundária
+	    if(matriz[0][2] == 'X'){
+	    	(*placarA)++;
+	    	return 1;
+		}else if(matriz[0][2] == 'O'){
+			(*placarB)++;
+			return 2;
+		}else{
+			return 3;
+		}
+	}
+	return 0;
+}
+
+void realizarJogada(char matriz[3][3], char *simbolo, int linha, int coluna, char contra) {
+    int jogada_Linha, jogada_Coluna;
+    printf("É a vez do jogador %c\n", *simbolo);
+    printf("Digite a posição na qual deseja jogar (ex: 0 2 [linha e coluna])");
+	scanf("%d %d", &jogada_Linha, &jogada_Coluna);
+	getchar();
+	if(matriz[jogada_Linha][jogada_Coluna] != ' '){
+		printf("Jogada inválida, tente novamente!\n");
+		printf("---------------------------------\n");
+	} else{
+		matriz[jogada_Linha][jogada_Coluna] = *simbolo;
+		if(contra == 'p'){
+			if(*simbolo == 'X'){
+				*simbolo = 'O';
+			} else{
+				*simbolo = 'X';
+			}	
+		} else{
+			*simbolo = 'X';
+		}
+	}
+}
+
+void jogadaMaquina(char matriz[3][3]) {
+    int linha, coluna;
+    do {
+        linha = rand() % 3;
+        coluna = rand() % 3;
+    } while (matriz[linha][coluna] != ' ');
+
+    matriz[linha][coluna] = 'O';
+    printf("A máquina jogou em %d %d\n", linha, coluna);
+}
+void pegarNomeJogador(char jogador1[50], char jogador2[50], char contra){
+	if (strcmp(jogador1, " ") == 0 && strcmp(jogador2, " ") == 0) {
+        printf("Digite o nome do 1º jogador (X): ");
+        scanf("%s", jogador1);
+        if (contra == 'p') {
+            printf("Digite o nome do 2º jogador (O): ");
+            scanf("%s", jogador2);
+        } else {
+            strcpy(jogador2, "Máquina");
+        }
+    }
+}
+
 int main(){
 	setlocale(0, "Portuguese");
 	char matriz[3][3] = {
@@ -42,223 +190,52 @@ int main(){
 	printf("---------------------------------\n");
 
 	do{
-		printf("O que você deseja fazer?\n");
-		printf("1- Jogar;\n2- Ver Ranking;\n3- Créditos;\n4- Sair.\n");
+		exibirMenu();
 		scanf("%d", &opcao);
 		getchar();
+		
 		switch (opcao){
 			case 1:
 				printf("Contra quem você deseja jogar? jogador:(p) máquina:(m)\n");
 				scanf("%c", &contra);
-//				getchar();
-				switch (contra){
-					case 'p':
-						//OPÇÃO DE JOGAR CONTRA OUTRO JOGADOR
-						//Reiniciando a matriz (criar uma função para isso depois)
-						for (int i = 0; i < 3; i++) {
-					        for (int j = 0; j < 3; j++) {
-					            matriz[i][j] = ' ';
-					        }
-					    }
-					    vencedor = 0;
+				
+				//Reiniciando a matriz:
+				inicializarMatriz(matriz);
 						
-						if(strcmp(jogador1, " ") == 0 && strcmp(jogador2, " ") == 0){
-							//Pegando os nomes dos jogadores:
-							printf("Digite o nome do 1º jogador (X):\n");
-							scanf("%s", jogador1);
-							printf("Digite o nome do 2º jogador: (O)\n");
-							scanf("%s", jogador2);
-						}
+				//Pegando os nomes dos jogadores
+				pegarNomeJogador(jogador1, jogador2, contra); 
+				
+				switch (contra){
+					case 'p': //OPÇÃO DE JOGAR CONTRA OUTRO JOGADOR
+						
 						
 						do{ //Laço de repetição que só finaliza quando tiver um vencedor
-							printf("É a vez do jogador %c\n", simbolo);
-							printf("Digite a posição na qual deseja jogar (ex: 0 2 [linha e coluna])");
-							scanf("%d %d", &jogada_Linha, &jogada_Coluna);
-							getchar();
-							if(matriz[jogada_Linha][jogada_Coluna] != ' '){
-								printf("Jogada inválida, tente novamente!\n");
-								printf("---------------------------------\n");
-							} else{
-								matriz[jogada_Linha][jogada_Coluna] = simbolo;
-								if(simbolo == 'X'){
-									simbolo = 'O';
-								} else{
-									simbolo = 'X';
-								}
-								for(int i = 0; i < 3; i++){
-									for(int j = 0; j < 3; j++){
-										if (matriz[i][j] == ' '){
-											printf("\t*");
-										}else{
-											printf("\t%c", matriz[i][j]);
-										}
-									}
-									printf("\n\n");
-								}
-							}
+							//Pegando a jogada dos jogadores
+							realizarJogada(matriz, &simbolo, jogada_Linha, jogada_Coluna, contra);
 							
-							// Verifica linhas e colunas caso o jogador 1 tenha vencido
-						    for (int i = 0; i < 3; i++) {
-						        if (matriz[i][0] == matriz[i][1] && matriz[i][1] == matriz[i][2] && matriz[i][0] != ' ') {
-						             // Linha completa
-						            if(matriz[i][0] == 'X'){
-						            	vencedor = 1;
-						            	placarA++;
-									}else if(matriz[i][0] == 'O'){
-										vencedor = 2;
-										placarB++;
-									}else{
-										vencedor = 4;
-									}
-						            
-						        }
-						        if (matriz[0][i] == matriz[1][i] && matriz[1][i] == matriz[2][i] && matriz[0][i] != ' ') {
-						            // Coluna completa
-						            if(matriz[0][i] == 'X'){
-						            	vencedor = 1;
-						            	placarA++;
-									}else if(matriz[0][i] == 'O'){
-										vencedor = 2;
-										placarB++;
-									}else{
-										vencedor = 4;
-									}
-						        }
-						    }
-						
-						    // Verifica diagonais
-						    if (matriz[0][0] == matriz[1][1] && matriz[1][1] == matriz[2][2] && matriz[0][0] != ' ') {
-						        // Diagonal principal
-						        if(matriz[0][0] == 'X'){
-						        	vencedor = 1;
-						        	placarA++;
-								}else if(matriz[0][0] == 'O'){
-									vencedor = 2;
-									placarB++;
-								}else{
-									vencedor = 4;
-								}
-						    }
-						    if (matriz[0][2] == matriz[1][1] && matriz[1][1] == matriz[2][0] && matriz[0][2] != ' ') {
-						        // Diagonal secundária
-						        if(matriz[0][2] == 'X'){
-						        	vencedor = 1;
-						        	placarA++;
-								}else if(matriz[0][2] == 'O'){
-									vencedor = 2;
-									placarB++;
-								}else{
-									vencedor = 4;
-								}
-						    }
-
+							//Mostra a tabela
+							exibirMatriz(matriz);
+							
+							// Verifica o vencedor e armazena o valor na variavel vencedor
+						    vencedor = verificarVencedor(matriz, &placarA, &placarB);
+						    
 						} while (vencedor == 0);
 						break;
 					case 'm':
 						//OPÇÃO DE JOGAR CONTRA A MÁQUINA (CPU)
-						printf("Digite o seu nome, jogador:\n");
-						scanf("%s", jogador1);
-						strcpy(jogador2, "Máquina");
-						for (int i = 0; i < 3; i++) {
-					        for (int j = 0; j < 3; j++) {
-					            matriz[i][j] = ' ';
-					        }
-					    }
 						do{
-							printf("É a vez do jogador %c\n", simbolo);
-							printf("Digite a posição na qual deseja jogar (ex: 0 2 [linha e coluna])");
-							scanf("%d %d", &jogada_Linha, &jogada_Coluna);
-							getchar();
-							if(matriz[jogada_Linha][jogada_Coluna] != ' '){
-								printf("Este local já está preenchido, tente novamente!\n");
-								printf("---------------------------------\n");
-							} else{
-								matriz[jogada_Linha][jogada_Coluna] = simbolo;
-								for(int i = 0; i < 3; i++){
-									for(int j = 0; j < 3; j++){
-										if (matriz[i][j] == ' '){
-											printf("\t*");
-										}else{
-											printf("\t%c", matriz[i][j]);
-										}
-									}
-									printf("\n\n");
-								}
-							}
+							//Pegando a jogada do jogador
+							realizarJogada(matriz, &simbolo, jogada_Linha, jogada_Coluna, contra);
 							
-							// Verifica linhas e colunas caso o jogador 1 tenha vencido
-						    for (int i = 0; i < 3; i++) {
-						        if (matriz[i][0] == matriz[i][1] && matriz[i][1] == matriz[i][2] && matriz[i][0] != ' ') {
-						             // Linha completa
-						            if(matriz[i][0] == 'X'){
-						            	vencedor = 1;
-						            	placarA++;
-									}else if(matriz[i][0] == 'O'){
-										vencedor = 2;
-										placarB++;
-									}else{
-										vencedor = 4;
-									}
-						            
-						        }
-						        if (matriz[0][i] == matriz[1][i] && matriz[1][i] == matriz[2][i] && matriz[0][i] != ' ') {
-						            // Coluna completa
-						            if(matriz[0][i] == 'X'){
-						            	vencedor = 1;
-						            	placarA++;
-									}else if(matriz[0][i] == 'O'){
-										vencedor = 2;
-										placarB++;
-									}else{
-										vencedor = 4;
-									}
-						        }
-						    }
-						
-						    // Verifica diagonais
-						    if (matriz[0][0] == matriz[1][1] && matriz[1][1] == matriz[2][2] && matriz[0][0] != ' ') {
-						        // Diagonal principal
-						        if(matriz[0][0] == 'X'){
-						        	vencedor = 1;
-						        	placarA++;
-								}else if(matriz[0][0] == 'O'){
-									vencedor = 2;
-									placarB++;
-								}else{
-									vencedor = 4;
-								}
-						    }
-						    if (matriz[0][2] == matriz[1][1] && matriz[1][1] == matriz[2][0] && matriz[0][2] != ' ') {
-						        // Diagonal secundária
-						        if(matriz[0][2] == 'X'){
-						        	vencedor = 1;
-						        	placarA++;
-								}else if(matriz[0][2] == 'O'){
-									vencedor = 2;
-									placarB++;
-								}else{
-									vencedor = 4;
-								}
-						    }
-						    if(vencedor == 0){
-						    	//JOGADA DA MÁQUINA
-								do {
-							        jogada_Linha = rand() % 3;  // Gera número entre 0 e 2
-							        jogada_Coluna = rand() % 3;
-							    } while (matriz[jogada_Linha][jogada_Coluna] != ' '); // Repete até encontrar um espaço vazio
-								matriz[jogada_Linha][jogada_Coluna] = 'O'; // Máquina joga 'O'
-								printf("A máquina jogou %d %d\n", jogada_Linha, jogada_Coluna);
-								for(int i = 0; i < 3; i++){
-									for(int j = 0; j < 3; j++){
-										if (matriz[i][j] == ' '){
-											printf("\t*");
-										}else{
-											printf("\t%c", matriz[i][j]);
-										}
-									}
-									printf("\n\n");
-								}
-							}
+							//Informando a jogada da máquina
+						    jogadaMaquina(matriz);
+						    
+							//Mostra a tabela
+							exibirMatriz(matriz);
+							
+							// Verifica o vencedor e armazena o valor na variavel vencedor
+						    vencedor = verificarVencedor(matriz, &placarA, &placarB);
+						    
 						} while (vencedor == 0);
 						
 						break;
@@ -267,19 +244,7 @@ int main(){
 				}
 				break;
 			case 2:
-				//OPCÇÃO DE VER RANKING
-				printf("---------------------------------\n");
-				printf("\t   Ranking:\n");
-				printf("---------------------------------\n");
-				if(strcmp(jogador1, " ") == 0){
-					printf("Você ainda não jogou nenhuma partida\n\n");
-					continue;
-				} else if (strcmp(jogador2, " ") == 0 && strcmp(jogador1, " ") != 0){
-					printf("%s: %d pontos\n", jogador1, placarA);
-				} else{
-					printf("%s: %d pontos\n", jogador1, placarA);
-					printf("%s: %d pontos\n", jogador2, placarB);
-				}
+ 				exibirRanking(jogador1, jogador2, placarA, placarB);
 				break;
 			case 3:
 				//OPÇÃO DE CRÉDITOS
@@ -302,8 +267,6 @@ int main(){
 			default:
 				printf("Digite uma opção válida");
 		}
-			
-//		getchar();
 		
 		switch (vencedor){
 		case 1:
@@ -313,9 +276,6 @@ int main(){
 			printf("Parabéns, %s. Você venceu!!\n", jogador2);
 			break;
 		case 3:
-			printf("A máquina venceu!\n");
-			break;
-		case 4:
 			printf("Essa não, deu velha!\n");
 			break;
 		default:
@@ -326,6 +286,4 @@ int main(){
 		scanf("%c", &continuar);
 		
 	} while (continuar == 's' || continuar == 'S');
-		
-	
 }
